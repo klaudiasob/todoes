fb834f3d9462158fa0ed1f72dd9cc6dcf0016020
klaudiasob <61837479+klaudiasob@users.noreply.github.com>
2020-09-29 19:43:51 +0200

    Create swagger documentation. (#8)

---
+++ spec/swagger_helper.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.configure do |config|
+  # Specify a root folder where Swagger JSON files are generated
+  # NOTE: If you're using the rswag-api to serve API descriptions, you'll need
+  # to ensure that it's configured to serve Swagger from the same folder
+  config.swagger_root = Rails.root.join('swagger').to_s
+
+  # Define one or more Swagger documents and provide global metadata for each one
+  # When you run the 'rswag:specs:swaggerize' rake task, the complete Swagger will
+  # be generated at the provided relative path under swagger_root
+  # By default, the operations defined in spec files are added to the first
+  # document below. You can override this behavior by adding a swagger_doc tag to the
+  # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
+  config.swagger_docs = {
+    'v1/swagger.yaml' => {
+      openapi: '3.0.1',
+      info: {
+        title: 'Todoes API',
+        version: 'v1',
+        description: 'This is the documentation for Version 1 (V1) of the Todoes API.
+          The API requires an bearer token for most endpoints. You can obtain an bearer token by signing up using POST /api/v1/users, then sign in using /users/sign_in.
+          In response, you will receive your unique bearer token which you will find it in response headers. '
+      },
+      components: {
+        securitySchemes: {
+          basic_auth: {
+            type: :http,
+            scheme: :bearer,
+            bearerFormat: JWT
+          }
+        }
+      },
+      paths: {}
+    }
+  }
+
+  # Specify the format of the output Swagger file when running 'rswag:specs:swaggerize'.
+  # The swagger_docs configuration option has the filename including format in
+  # the key, this may want to be changed to avoid putting yaml in json files.
+  # Defaults to json. Accepts ':json' and ':yaml'.
+  config.swagger_format = :yaml
+end
